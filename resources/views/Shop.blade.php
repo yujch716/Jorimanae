738dafc9c322926c30f74b514dccbd6c9da1d501
yujch716 <yujch716@gmail.com>
2022-09-25 06:54:33 +0900

    20220925

--- resources/views/Shop.blade.php
+++ resources/views/Shop.blade.php
@@ -46,16 +46,13 @@
 <!-- Hero Section End -->
 
 <!-- Breadcrumb Section Begin -->
-<section class="breadcrumb-section set-bg" data-setbg="img/breadcrumb.jpg">
+<section class="breadcrumb-section set-bg" data-setbg="img/background/shop.jpg">
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
-                <div class="breadcrumb__text">
-                    <h2>Organi Shop</h2>
-                    <div class="breadcrumb__option">
-                        <a href="./index.html">Home</a>
-                        <span>Shop</span>
-                    </div>
+                <div class="breadcrumb__text2">
+                    <h2>Shop</h2>
+                    <br><br>
                 </div>
             </div>
         </div>
